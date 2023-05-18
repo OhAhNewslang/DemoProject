8a1bd9856f8ffe9273c9c2cf1706e783d6ad824d
jin5eok5 <ojs258@naver.com>
2023-05-18 16:11:41 +0900

    2023-05-18 csr 실패

--- src/main/java/com/example/DemoProject/controller/CsrTestController.java
+++ src/main/java/com/example/DemoProject/controller/CsrTestController.java
@@ -1,10 +1,8 @@
 package com.example.DemoProject.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.yaml.snakeyaml.util.ArrayStack;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,7 +10,7 @@ import java.util.List;
 @RestController
 public class CsrTestController {
     @GetMapping("hello1")
-    public String helloWorld(){
-        return "OHAH! ProtoType CSR Test Hello World ~!";
+    public String helloWorld(Model model){
+        return "OHAH! ProtoType CSR Test HelloWorld ~!";
     }
 }
