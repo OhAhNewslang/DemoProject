9b420896eca8a7c43335896e753056a6b65f8c56
jin5eok5 <ojs258@naver.com>
2023-05-18 16:09:13 +0900

    2023-05-18 csr실패

--- frontend/pages/hello1.tsx
+++ frontend/pages/hello1.tsx
@@ -1,8 +1,8 @@
-import axios from 'axios'
+import axios from "axios";
 
-export default function callSsr() {
-    const result = axios.get("/hello1")
-    .then(res => {
-        console.log(res.data)
-    })
+export default function(){
+  axios.get('http://localhost:8080/hello1')
+  .then(res => {
+  console.log(res.data)
+})
 }
