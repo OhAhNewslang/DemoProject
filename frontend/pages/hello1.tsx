2d919f2900fcb31877de5c3e256f20e55ad98a37
jin5eok5 <ojs258@naver.com>
2023-05-20 21:29:49 +0900

    2023-05-20 csr...

--- frontend/pages/hello1.tsx
+++ frontend/pages/hello1.tsx
@@ -1,2 +1,28 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+
+const Csr = () => {
+    const [text, setText] = useState<String>();
+
+    useEffect(() => {
+        getFetch();
+    });
+
+    const getFetch = async () => {
+        try{
+            const res = await axios.get('/hello1');
+            const text : String = res.data;
+
+            setText(text);
+
+        } catch(error) {
+            console.log(error);
+        }
+    };
+    return(
+        <div>
+            {text}
+        </div>
+    )
+}
+export default Csr;
